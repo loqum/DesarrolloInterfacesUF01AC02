7b0dc57dae08dec6ebba4958f73140012643f5ba
Ruben <ruben693@gmail.com>
2018-10-12 17:17:08 +0200

    Initial commit

---
+++ app/src/main/java/rfm/com/desarrollointerfacesuf01ac02/Formulario.java
@@ -0,0 +1,13 @@
+package rfm.com.desarrollointerfacesuf01ac02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Formulario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
